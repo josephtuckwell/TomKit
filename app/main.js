191a6d26246ad16e48b7d5f1de6a2491b57ce451
Joseph Tuckwell <joetuckwell@icloud.com>
2025-08-13 18:52:36 +0100

    Added code comments

--- app/main.js
+++ app/main.js
@@ -2,39 +2,50 @@ const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
 const { execFile } = require('child_process');
 
+// Create the main application window
 const createWindow = () => {
   const win = new BrowserWindow({
     webPreferences: {
+      // Preload script to securely expose APIs to renderer
       preload: path.join(__dirname, 'preload.js')
     }
   })
 
+  // Load the main HTML file
   win.loadFile('index.html')
 }
 
+// Called when Electron has finished initialization
 app.whenReady().then(() => {
   createWindow()
 
+  // On macOS, re-create a window when the dock icon is clicked and there are no other windows open
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
 
+// Quit the app when all windows are closed (except on macOS)
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit()
 })
 
+// Handle "start-tomcat" requests from the renderer process
 ipcMain.handle('start-tomcat', async (event, installPath, type) => {
   let scriptPath = '';
+  let args;
 
+  // If using Homebrew, start Tomcat as a service
   if (type !== 'Homebrew') {
     scriptPath = 'brew';
     args = ['services', 'start', 'tomcat'];
   } else {
+    // Otherwise, run the startup.sh script from the manual install path
     scriptPath = path.join(installPath, 'startup.sh');
     args = [];
   }
 
+  // Execute the appropriate command/script
   execFile(scriptPath, args, (error, stdout, stderr) => {
     if (error) {
       console.error('Error starting Tomcat:', error);
@@ -44,17 +55,22 @@ ipcMain.handle('start-tomcat', async (event, installPath, type) => {
   });
 });
 
+// Handle "stop-tomcat" requests from the renderer process
 ipcMain.handle('stop-tomcat', async (event, installPath, type) => {
   let scriptPath = '';
+  let args;
 
+  // If using Homebrew, stop Tomcat as a service
   if (type !== 'Homebrew') {
     scriptPath = 'brew';
     args = ['services', 'stop', 'tomcat'];
   } else {
+    // Otherwise, run the shutdown.sh script from the manual install path
     scriptPath = path.join(installPath, 'shutdown.sh');
     args = [];
   }
 
+  // Execute the appropriate command/script
   execFile(scriptPath, args, (error, stdout, stderr) => {
     if (error) {
       console.error('Error stopping Tomcat:', error);
--- app/preload.js
+++ app/preload.js
@@ -1,6 +1,8 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('api', {
-  startTomcat: (path) => ipcRenderer.invoke('start-tomcat', path),
-  stopTomcat: (path) => ipcRenderer.invoke('stop-tomcat', path)
+
+  // Tell renderer to start or stop Tomcat by sending a message to main process
+  startTomcat: (path, type) => ipcRenderer.invoke('start-tomcat', path, type),
+  stopTomcat: (path, type) => ipcRenderer.invoke('stop-tomcat', path, type)
 });
